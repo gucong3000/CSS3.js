c771e97c5dfdf08ae775384a82addaa3e1ebc5c3
Ridge <ridge.liu@toursforfun.com>
2015-01-14 14:39:11 +0800

    placeholder

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 CSS3.js
 =======
 
-CSS3 polyfill for IE
+CSS3 polyfill
+
+## CSS3.js 的用途
+
+- IE下兼容圆角、阴影等css3属性()
+- css3 免私有前缀(`-webkit-`、`-moz-`等)
+- css3 相对长度单位兼容(`vh`、`vw`、`vmax`、`vmin`、`rem`)
+- css2 绝对长度单位适应移动端(`cm`、`mm`、`in`、`pt`、`pc`、`q`)
+- @media 兼容低端浏览器(`@media screen and (width:800px){ … }`)
+
---
+++ src/placeholder.js
@@ -0,0 +1,286 @@
+/**
+ * @class h5form
+ */
+
+(function(window, document, undefined) {
+
+	"use strict";
+
+	var strPlaceholder = "placeholder",
+		styleRules = strPlaceholder + "{position:absolute;cursor:text;color:gray;padding:0;border:0;overflow:hidden;-ms-user-select:none;user-select:none;pointer-events:none;}textarea{overflow: auto;}",
+		attrName = "data-" + strPlaceholder + new Date().getTime() + Math.random(),
+		strDOMAttrModified = "DOMAttrModified",
+		root = document.documentElement,
+		head = root.children[0],
+		parseInt = window.parseInt,
+		supportDOMAttrModified,
+		placeholderCache = {},
+		strNormal = "normal",
+		strStatic = "static",
+		strPx = "px",
+		styleNode,
+		getComputedStyle = window.getComputedStyle ? function(node) {
+			return window.getComputedStyle(node, null);
+		} : 0;
+
+	// document.createElement  快捷方式
+	function createElement(tagName) {
+		return document.createElement(tagName);
+	}
+
+	//判断对象是否文本框
+	function isTextbox(node) {
+		return /^text(area)?|password|email|search|tel|url$/i.test(node.type);
+	}
+
+	//设置节点的holder
+	function setPlaceholder(node, value) {
+		if (node.uniqueID) {
+			placeholderCache[node.uniqueID] = value;
+		} else {
+			node[attrName] = value;
+		}
+	}
+
+	//获取节点的holder状态
+	function getPlaceholder(node) {
+		var value;
+		if (node.uniqueID) {
+			value = placeholderCache[node.uniqueID];
+			placeholderCache[node.uniqueID] = value || true;
+		} else {
+			value = node[attrName];
+			node[attrName] = value || true;
+		}
+		return value;
+	}
+
+	//获取node的style对象，优先使用runtimeStyle
+	function runtimeStyle(node) {
+		return node.runtimeStyle || node.style;
+	}
+
+	//获取node的计算样式，兼容IE，非IE
+	function currentStyle(node) {
+		return node.currentStyle || getComputedStyle(node);
+	}
+
+	function forEach(array, fn) {
+		if (array) {
+			[].slice.call(array, 0).forEach(fn);
+		}
+	}
+
+	function addEventListener(node, type, listener, useCapture) {
+		node.addEventListener(type, listener, !!useCapture);
+	}
+
+	/**
+	 * @method placeholder
+	 * @param {HTMLInputElement | HTMLTextAreaElement} node 需要修正placeholder的DOM元素
+	 * @description 修正或更新文本框的placeholder功能 默认只在低端浏览器下使用
+	 */
+
+	//为input建立模拟的placeholder
+	function createHolder(input) {
+		var currStyle = currentStyle(input),
+			holder = getPlaceholder(input),
+			timer,
+			on = function(eType, fn, node) {
+				addEventListener(node || input, eType, fn, true);
+			},
+			//更新placeholder文本
+			setText = function() {
+				//读取placeholder
+				var text = input[strPlaceholder];
+				//如果placeholder属性不为空而node还没有建立
+				if ((!holder || !holder.tagName) && text) {
+					//建立一个node
+					holder = createElement(strPlaceholder);
+					holder.onmousedown = function() {
+						//鼠标点holder是文本框获得焦点
+						setTimeout(function() {
+							input.focus();
+						}, 1);
+						return false;
+					};
+					setPlaceholder(input, holder);
+				}
+				//如果有node，更新其内容为placeholder属性值
+				if (holder && holder.tagName) {
+					holder.innerHTML = text || "";
+				}
+			},
+			//控制node的样式
+			setDisplay = function() {
+				clearTimeout(timer);
+				if (holder && holder.tagName) {
+					var show = holder.innerHTML && !input.value && isTextbox(input),
+						style = runtimeStyle(holder),
+						parent = input.parentNode,
+						disp = parent && (input.offsetHeight || input.offsetWidth);
+					style.display = show && disp ? "block" : "none";
+
+					//如果文本框可见时
+					if (!disp) {
+						//文本框不可见时延迟运行setDisplay
+						timer = setTimeout(setDisplay, 50);
+					} else if (show) {
+						timer = setTimeout(function() {
+							if (currStyle.position === strStatic && currentStyle(parent).position === strStatic) {
+								runtimeStyle(input).position = "relative";
+							}
+							if (parent === input.offsetParent && !document.querySelector && /^normal$/i.test(currentStyle(parent).zoom)) {
+								runtimeStyle(parent).zoom = 1;
+							}
+							//如果文本框或其父元素定位不为static，则自动计算placeholder的位置
+							style.maxWidth = getComputedStyle && !/^auto$/.test(currStyle.width) ? currStyle.width : (input.clientWidth - parseInt(currStyle.paddingLeft) - parseInt(currStyle.paddingRight)) + strPx;
+							style.width = "XMLHttpRequest" in window && currStyle.textAlign === "left" ? "auto" : style.maxWidth;
+							style.left = (input.offsetLeft + input.clientLeft) + strPx;
+							style.top = (input.offsetTop + input.clientTop) + strPx;
+							currCss("marginLeft", "paddingLeft");
+							currCss("marginTop", "paddingTop");
+
+							if (/^input$/i.test(input.tagName)) {
+								style.whiteSpace = "nowrap";
+								style.wordBreak = strNormal;
+								if (getComputedStyle) {
+									style.lineHeight = getComputedStyle(input).height;
+								} else {
+									currCss("lineHeight");
+								}
+							} else {
+								style.whiteSpace = strNormal;
+								style.wordBreak = "break-all";
+								//style.wordWrap = "break-word";
+								currCss("lineHeight");
+							}
+
+							currCss("textAlign");
+							currCss("textIndent");
+							currCss("fontFamily");
+							//currCss("fontWidth");
+							currCss("fontSize");
+
+							//将node插入文本框之后
+							if (input.nextSibling) {
+								parent.insertBefore(holder, input.nextSibling);
+							} else {
+								parent.appendChild(holder);
+							}
+						}, 0);
+
+					}
+				}
+			},
+			//样式继承，取文本框的样式赋值给placeholder
+			currCss = function(name, attr) {
+				try {
+					runtimeStyle(holder)[name] = currentStyle(input)[attr || name];
+				} catch (e) {}
+			};
+
+		if (!holder) {
+			try {
+
+				//高级浏览器下事件注册
+				forEach(["input", "change", "keypress", strDOMAttrModified], function(eType) {
+					on(eType, function() {
+						setTimeout(function() {
+							setText();
+							setDisplay();
+						}, 0);
+					});
+				});
+
+				forEach(["resize", "scroll"], function(eType) {
+					on(eType, setDisplay, window);
+				});
+
+			} catch (ex) {
+				window.attachEvent("onresize", setDisplay);
+				input.attachEvent("onpropertychange", function() {
+					var propName = event.propName;
+					setTimeout(function() {
+						switch (propName) {
+							//如placeholder属性发生改变，重置文案和样式
+							case strPlaceholder:
+								setText();
+								//如value属性发生改变，重置重置样式
+								/* falls through */
+							default:
+								setDisplay();
+						}
+					}, 0);
+				});
+			}
+		} else if (supportDOMAttrModified) {
+			return;
+		}
+		//初始化placeholder及其样式
+		setText();
+		setDisplay();
+	}
+
+	function init() {
+		forEach(document.querySelectorAll("input,textarea"), createHolder);
+	}
+
+	try {
+		if (/interactive|complete/.test(document.readyState)) {
+			init();
+		} else {
+			addEventListener(document, "DOMContentLoaded", init);
+		}
+		setInterval(init, 200);
+
+		//IE 10+、Safari中placeholder在文本框focus时则消失，这与其他浏览器有差异，用css干掉其原生的placeholder功能
+		forEach([":-ms-input-", "::-webkit-input-"], function(prefix) {
+			styleRules += prefix + strPlaceholder + "{color:transparent !important;}";
+		});
+
+		(function() {
+			var id = root.id;
+
+			function fn() {
+				root.removeEventListener(strDOMAttrModified, fn, false);
+				supportDOMAttrModified = true;
+			}
+
+			addEventListener(root, strDOMAttrModified, fn);
+			root.id = "mass"; //更新属性
+			root.id = id; //无论如何也还原它
+		})();
+	} catch (ex) {
+		(function(init) {
+			var $ = window.$;
+			if ($) {
+				init($);
+			} else {
+				require.async(["jquery"], init);
+			}
+		})(function($) {
+			$(function() {
+				$("input,textarea").each(function() {
+					createHolder(this);
+				});
+			});
+		});
+	}
+
+	try {
+		styleNode = createElement("<style>" + styleRules + "</style>");
+	} catch (e) {
+		styleNode = createElement("style");
+		styleNode.textContent = styleRules;
+	}
+
+	head.insertBefore(styleNode, head.firstChild);
+
+	try {
+		module.exports = createHolder;
+	} catch (e) {
+		window.placeholder = createHolder;
+	}
+
+})(this, document);
