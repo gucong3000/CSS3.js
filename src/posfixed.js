c5936d6f39e5cc393c07278c102fbbaccfce177a
Ridge <ridge.liu@toursforfun.com>
2015-05-06 19:07:09 +0800

    up

--- src/posfixed.js
+++ src/posfixed.js
@@ -1,12 +1,22 @@
 "use strict";
 (function(window) {
 
-	var expression = "$1:expression(seajs.require(\"posfixed\")(this,\"$1\",\"$2\"))$3",
+	var expression = express() + "$3",
 		StyleFix = window.stylefix || require("stylefix"),
 		cssprops = window.cssprops || require("cssprops"),
+		document = window.document,
 		html = document.documentElement,
+		fixedhelper = "fixedhelper_" + document.uniqueID,
+		head = html.childNodes[0],
 		ieVersion = StyleFix.ieVersion,
-		cssValCache = {};
+		cssValCache = {},
+		style;
+
+	function express(propName, propVlaue) {
+		propName = propName || "$1";
+		propVlaue = propVlaue || "$2";
+		return propName + ":expression(seajs.require(\"posfixed\")(this,\"" + propName + "\",\"" + propVlaue + "\"))";
+	}
 
 	function posfixed(element, propName, propVlaue) {
 		// uniqueID：IE的特有属性，表示dom唯一标识
@@ -25,17 +35,21 @@
 			cssValCache[uniqueID] = cssCache = {};
 		}
 
-		if (propName === "position" && propVlaue === "fixed") {
-			var left = parseInt(cssCache.left),
-				top = parseInt(cssCache.top),
-				right = parseInt(cssCache.right),
-				bottom = parseInt(cssCache.bottom);
-			cssCache.fixedleft = isNaN(left) ? (isNaN(right) ? cssCache.left : html.scrollLeft + html.clientWidth - element.offsetWidth - right) : html.scrollLeft + left;
-			cssCache.fixedright = "auto";
-			cssCache.fixedtop = isNaN(top) ? (isNaN(bottom) ? cssCache.top : html.scrollTop + html.clientHeight - element.offsetHeight - bottom) : html.scrollTop + top;
-			cssCache.fixedbottom = "auto";
+		if (propName === "position") {
+			if (propVlaue === "fixed") {
+				if (!cssCache.fixedright) {
+					element.className += " " + fixedhelper;
+					cssCache.fixedright = cssCache.fixedbottom = "auto";
+				}
+				var left = parseInt(cssCache.left),
+					top = parseInt(cssCache.top),
+					right = parseInt(cssCache.right),
+					bottom = parseInt(cssCache.bottom);
+				cssCache.fixedleft = isNaN(left) ? (isNaN(right) ? cssCache.left : html.scrollLeft + html.clientWidth - element.offsetWidth - right) : html.scrollLeft + left;
+				cssCache.fixedtop = isNaN(top) ? (isNaN(bottom) ? cssCache.top : html.scrollTop + html.clientHeight - element.offsetHeight - bottom) : html.scrollTop + top;
 
-			returnValue = "absolute";
+				returnValue = "absolute";
+			}
 		} else if (cssCache.position === "fixed") {
 			returnValue = cssCache["fixed" + propName];
 		}
@@ -57,6 +71,14 @@
 			/^(left|top|right|bottom)\s*:\s*([\d\.+]*\w*)([\};]|$)/i,
 			expression
 		]);
+
+		StyleFix.ready(function() {
+			style = document.createElement("style");
+			head.insertBefore(style, head.firstChild);
+			StyleFix.cssContent(style, "html,body{background: url(about:blank) no-repeat fixed}." + fixedhelper + "{" + express("top", "auto") + ";" + express("left", "auto") + ";" + express("right", "auto") + ";" + express("bottom", "auto") + "}");
+		});
+
+
 		//暴露出的接口
 		try {
 			module.exports = posfixed;
