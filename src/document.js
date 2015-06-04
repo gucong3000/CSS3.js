b287f890464ad54c1463a09d7828409dfbcc37b5
Ridge <ridge.liu@toursforfun.cn>
2015-06-04 17:02:22 +0800

    fix @document

--- src/document.js
+++ src/document.js
@@ -4,8 +4,29 @@
 		document = window.document,
 		html = document.documentElement,
 		StyleFix = window.stylefix || require("stylefix"),
-		prefixfree = window.prefixfree || require("prefixfree");
+		prefixfree = window.prefixfree || require("prefixfree"),
+		docFns = {
+			url: function(url) {
+				// 匹配整个URL
+				return location.href === url;
+			},
+			"url-prefix": function(url) {
+				// 匹配文档的URL是否以参数指定的值开头
+				return regTest("^" + url);
+			},
+			domain: function(domain) {
+				// 匹配文档的域名是否为参数中指定的域名或者为它的子域名
+				return regTest("(^|.*?\\.)" + domain + "$", location.hostname);
+			},
+			regexp: function(regexp) {
+				// 匹配文档的URL是否和参数中指定的正则表达式匹配.该表达式必须匹配整个URL.
+				return regTest(regexp.replace(/([\"\'])(.+)\1/, "^$2$"));
+			}
+		};
 
+	function regTest(regexp, string) {
+		return new RegExp(regexp).test(string || location.href);
+	}
 
 	// 测试浏览器是否原生支持@support指令
 	function support() {
@@ -23,53 +44,11 @@
 		return result;
 	}
 
-	function _CSS_doc() {
-		var result = true;
-		for (var i = arguments.length - 1; i >= 0; i--) {
-			result = result && _documentCondition(arguments[i]);
-		}
-		return result;
-	}
-
-	// _documentCondition("url() url-prefix() domain() regexp()")
-	function _documentCondition(str) {
-		var result,
-			rule_type,
-			rule_content,
-			url_href = location.href;
-
-		if (str) {
-			rule_type = str.substr(0, str.indexOf("("));
-			rule_content = (str.match(/\((.*?)\)/))[1];
-			switch (rule_type) {
-				case "url":
-					if (url_href === rule_content) {
-						result = true;
-					}
-					break;
-				case "url-prefix":
-					var url_prefix = url_href.substr(0, (url_href.lastIndexOf("/") + 1));
-					if (url_prefix === rule_content) {
-						result = true;
-					}
-					break;
-				case "domain":
-					var url_hostname = location.hostname;
-					if (url_hostname.match(rule_content)) {
-						result = true;
-					}
-					break;
-				case "regexp":
-					rule_content = rule_content.substr(1, (rule_content.length - 2));
-					if (url_href.match(rule_content)) {
-						result = true;
-					}
-					break;
-				default:
-					console.log("there no document solution like you write!");
-			}
-			return result || false;
-		}
+	function _document(strRules) {
+		return strRules.split(/\s*,\s*(?=[\w\-]+\()/).some(function(rule) {
+			rule = rule.match(/(.+)\(\s*(.+)\s*\)/);
+			return docFns[rule[1]](rule[2]);
+		});
 	}
 
 	if (!support()) {
@@ -77,7 +56,7 @@
 			if (raw) {
 				return css.replace(/@document\s+([^\{]+)/g, function(str, strRules) {
 					try {
-						if (strRules && _CSS_doc(strRules)) {
+						if (strRules && _document(strRules)) {
 							str = "@media all ";
 						}
 					} catch (ex) {
@@ -87,6 +66,10 @@
 				});
 			}
 		});
+		if (window.addEventListener) {
+			window.addEventListener("popstate", function() {
+				StyleFix.process();
+			}, false);
+		}
 	}
-
 })(window);
